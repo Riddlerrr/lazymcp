e9b1d2f6297c0a1d7a16fc862779d19945571d5f
Sergey Andronov <sergey@andronov.me>
2025-07-19 15:51:02 +0200

    Website: Setup simple main page

--- www/vocs.config.ts
+++ www/vocs.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
-  title: 'Docs',
+  title: 'LazyMCP',
+  description: 'A general-purpose MCP (Model Context Protocol) server written in Go that provides real-time data ',
   sidebar: [
     {
       text: 'Getting Started',
       link: '/getting-started',
-    },
-    {
-      text: 'Example',
-      link: '/example',
     },
   ],
 })
