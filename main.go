3d55bf5c10929c7ac200a6600fc53416607b8913
Sergey Andronov <sergey@andronov.me>
2025-07-20 17:36:12 +0200

    Add some logging

--- main.go
+++ main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/Riddlerrr/lazymcp/tools"
+	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
 )
 
-func main() {
-	s := server.NewMCPServer(
+func NewMCPServer() *server.MCPServer {
+	hooks := &server.Hooks{}
+
+	hooks.AddOnSuccess(func(ctx context.Context, id any, method mcp.MCPMethod, message any, result any) {
+		log.Printf("Success: %s, %v, %v, %v\n", method, id, message, result)
+	})
+
+	hooks.AddOnError(func(ctx context.Context, id any, method mcp.MCPMethod, message any, err error) {
+		log.Printf("Error: %s, %v, %v, %v\n", method, id, message, err)
+	})
+
+	hooks.AddOnRegisterSession(func(ctx context.Context, session server.ClientSession) {
+		log.Printf("onRegisterSession: %s\n", session.SessionID())
+	})
+
+	mcpServer := server.NewMCPServer(
 		"LazyMCP",
 		"1.0.0",
 		server.WithToolCapabilities(false),
-		server.WithLogging(),
 		server.WithRecovery(),
+		server.WithLogging(),
+		server.WithHooks(hooks),
 	)
 
 	calculator := tools.NewCalculatorTool()
-	s.AddTool(calculator.Tool, calculator.Handler)
+	mcpServer.AddTool(calculator.Tool, calculator.Handler)
+	return mcpServer
+}
+
+func main() {
+	s := NewMCPServer()
 
 	// Create HTTP transport server
 	httpServer := server.NewStreamableHTTPServer(s)
